9a93b0277f5efd687309102e1607711a5a3876da
eysispeisi <eysteinntk@gmail.com>
2017-01-08 18:27:38 +0000

    use python unittest to test python bindings

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 import random
-import time
 import cSort
-import os
-import sys
+import unittest
 
-
-n = 100000
-base_list = range(n)
-random.shuffle(base_list)
+NO_ELEMENTS = 1000
 
 def is_sorted(lst):
     for i in range(1, len(lst)):
@@ -17,26 +12,73 @@ def is_sorted(lst):
     return True
 
 
-def dotest(name, func):
-    lst = base_list[:]
-    print "%s: "%name,
-    sys.stdout.flush()
-    t = time.time()
-    func(lst)
-    td = time.time()-t
-    s = is_sorted(lst)
-    print "sorted=%d, runtime=%f" % (s, td)
+class TestSortPython(unittest.TestCase):
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        self.sort_func = list.sort
+
+    def test_00(self):
+        '''Sort already sorted list of unique elements'''
+        lst = range(NO_ELEMENTS)
+        self.assertTrue(is_sorted(lst))
+        self.sort_func(lst)
+        self.assertTrue(is_sorted(lst))
+
+    def test_01(self):
+        '''Sort randomized list of elements: [0-9]{1}'''
+        lst = []
+        for i in range(NO_ELEMENTS):
+            lst.append(random.randint(0, 9))
+        self.assertFalse(is_sorted(lst))
+        self.sort_func(lst)
+        self.assertTrue(is_sorted(lst))
+
+    def test_02(self):
+        '''Sort randomized list of unique elements'''
+        lst = range(NO_ELEMENTS)
+        random.shuffle(lst)
+        self.assertFalse(is_sorted(lst))
+        self.sort_func(lst)
+        self.assertTrue(is_sorted(lst))
+
+    def test_03(self):
+        '''Sort list with first and last elements switched'''
+        lst = range(NO_ELEMENTS)
+        lst[0], lst[-1] = lst[-1], lst[0]
+        self.assertFalse(is_sorted(lst))
+        self.sort_func(lst)
+        self.assertTrue(is_sorted(lst))
+
+    def test_03(self):
+        '''Sort list with first at end of list'''
+        lst = range(NO_ELEMENTS)
+        tmp = lst.pop(0)
+        lst.append(tmp)
+        self.assertFalse(is_sorted(lst))
+        self.sort_func(lst)
+        self.assertTrue(is_sorted(lst))
 
 
-tests = [
-    ["list.sort()", list.sort],
-    ["cSort.quick()", cSort.quick],
-    ["cSort.merge()", cSort.merge],
-    ["cSort.heap()", cSort.heap],
-#    ["cSort.bubble()", cSort.bubble], # too slow to keep in active test
-    ]
+class TestSortQuick(TestSortPython):
+    def setUp(self):
+        self.sort_func = cSort.quick
+
+class TestSortMerge(TestSortPython):
+    def setUp(self):
+        self.sort_func = cSort.merge
+
+class TestSortHeap(TestSortPython):
+    def setUp(self):
+        self.sort_func = cSort.heap
+
+class TestSortBubble(TestSortPython):
+    def setUp(self):
+        self.sort_func = cSort.bubble
 
 
-for name, func in tests:
-    dotest(name, func)
+if __name__ == '__main__':
+    unittest.main()
 
